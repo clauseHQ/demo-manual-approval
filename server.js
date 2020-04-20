13ea96a39eb283ebaec890cbbe91afbbf9049f2a
Matt Roberts <code@rbrts.uk>
2020-04-20 19:28:18 +0100

    chore: MVP

--- server.js
+++ server.js
@@ -3,26 +3,38 @@ const bodyParser = require('body-parser')
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const flash = require('connect-flash');
 const helmet = require('helmet');
+const cors = require('cors');
 const path = require('path');
-const { JWK, JWE } = require('jose');
+const { JWK, JWE, JWT } = require('jose');
 const dotenv = require('dotenv');
+const axios = require('axios');
 dotenv.config();
 
 const {
   SHARED_ENCRYPTION_KEY,
-  COOKIE_SECRET
+  COOKIE_SECRET,
+  CLAUSE_API_HOST
 } = process.env;
 
 const key = JWK.asKey(SHARED_ENCRYPTION_KEY);
-const encrypt = object => JWE.encrypt(JSON.stringify(object), key);
 const decrypt = token => JSON.parse(JWE.decrypt(token, key).toString('utf8'));
+const sign = object => JWT.sign(object, key, {
+  expiresIn: '20m',
+  // algorithm: 'RS256',
+  // subject: '',
+  // audience: '',
+  issuer: 'https://approval.clause.io'
+});
 
 const app = express();
 app.use(helmet());
 app.use(logger('dev'));
 app.use(cookieParser());
+
+if (app.get('env') === 'development') {
+  app.use(cors());
+ }
 
 // config express-session
 var sess = {
@@ -39,26 +51,11 @@ if (app.get('env') === 'production') {
 }
 
 app.use(session(sess));
-// app.use(flash());
-
-// // Handle auth failure error messages
-// app.use(function (req, res, next) {
-//   if (req && req.query && req.query.error) {
-//     req.flash('error', req.query.error);
-//   }
-//   if (req && req.query && req.query.error_description) {
-//     req.flash('error_description', req.query.error_description);
-//   }
-//   next();
-// });
-
-app.get('/ping', (req, res) => {
- return res.send('pong');
-});
 
 app.get('/', (req, res) => {
   if (req.query.token) {
     req.session.data = decrypt(req.query.token);
+    req.session.token = req.query.token;
   }
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
@@ -69,12 +66,33 @@ app.get('/context', (req, res) => {
   return res.json(req.session.data || {});
 })
 
-app.post('/continue', (req, res) => {
+app.post('/continue', bodyParser.json(), (req, res) => {
   if (!req.session.data) {
     return res.status(403).send('Unauthorized');
   }
-  console.log(req.session.data);
-  res.json({ success: true });
+  axios({
+    method: 'post',
+    url: `${CLAUSE_API_HOST}/flows/apps/${req.session.data.appId}`,
+    data: { 
+      decodedToken: req.session.data,
+      appData: {
+        "$class": "org.accordproject.acceptanceofdelivery.InspectDeliverable",
+        "deliverableReceivedAt": new Date().toISOString(),
+        "inspectionPassed": req.body.approved,
+      },
+    },
+    headers: {
+      Authorization:'Bearer ' + sign({
+        appId: req.session.data.appId
+      })
+    }
+  }).then(response => {
+    res.json({ success: true });
+  }).catch(error => {
+    console.error(error.toJSON());
+    res.status(error.response.status).json({ success: false });
+  });
+
  });
  
 
--- src/App.js
+++ src/App.js
@@ -1,36 +1,72 @@
 import React, { useState, useEffect } from 'react';
 
-import { Form, TextArea, Button, Header } from 'semantic-ui-react'
+import { Button, Header, Loader, Dimmer } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
 const App = () => {
   const [context, setContext] = useState(null);
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(null);
+
   useEffect(() => {
+    setLoading(true);
     fetch('/context', {
       credentials: 'include'
     }).then(response => response.json())
-    .then(json => setContext(json))
+    .then(json => {
+      setContext(json);
+      setLoading(false);
+    });
   }, []);
 
-  // const token = new URLSearchParams(window.location.search).get('token');
-  
+  const handleClick = approved => {
+    setLoading(true);
+    fetch('/continue', {
+      method: 'post',
+      credentials: 'include',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ approved }),
+    })
+    .then(response => response.json())
+    .then(json => {
+      setResult(json.success);
+      setLoading(false);
+    });
+  };
+
+  if(result !== null){
+    return (
+      <div className="App">
+        <Header as='h3'>Manual Approval Step</Header>
+        { result 
+        ? <p>Your response was recorded.</p>
+        : <p>An error occured while recording your response.</p>
+        }
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Header>Manual Approval Step</Header>
-        <Form>
-          <TextArea value={context} />
-          <TextArea placeholder='Comments' />
-          <Button onClick={() => 
-          fetch('/continue', {
-            method: 'post',
-            credentials: 'include'
-          }).then(response => {
-            console.log(response.status);
-          })}>Approve</Button>
-        </Form>
-      </header>
+      <Header as='h3'>Manual Approval Step</Header>
+      { context && context.description
+      ? <div> 
+        <Dimmer active={loading}>
+          <Loader>Loading</Loader>
+        </Dimmer>
+        <p>{context.description}</p>
+        <Button color='red' onClick={() => handleClick(false)}>
+          Reject
+        </Button>
+        <Button color='green' onClick={() => handleClick(true)}>
+          Approve
+        </Button>
+      </div>
+      : <p>This link has expired or is invalid.</p>
+      }
     </div>
   );
 }
